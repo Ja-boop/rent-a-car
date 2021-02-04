979775878ee4aa6a07c6a74ee082635482baf1c2
Ja-boop <agusmisiones@gmail.com>
2021-02-04 17:56:16 -0300

    Base de datos creada

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ router.get('/', (req, res, next) => {
     res.render('home.njk', { logo: "/logo/logo-luzny.png", github: "https://github.com/Ja-boop/crud-autos" })
 });
 
+// user authentication
 router.get('/signup', (req, res, next) => {
 
 });
@@ -20,5 +21,8 @@ router.get('/login', (req, res, next) => {
 router.post('/login', (req, res, next) => {
     
 });
+// user authentication
+
+// rent a car
 
 module.exports = router;
